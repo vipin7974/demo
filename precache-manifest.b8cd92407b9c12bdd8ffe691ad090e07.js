878d5a1dc6a951aa779a27a875c6a4a9aa450708
vipin7974 <viprock234@gmail.com>
2020-08-06 10:40:09 +0530

    Updates

---
+++ precache-manifest.b8cd92407b9c12bdd8ffe691ad090e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036ea314012c2ce9c3cb04d6aa6a7894",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "485493e87e3d3b41bc40",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6afaee952689a6c54afd",
+    "url": "/demo/static/js/2.dfeba788.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.dfeba788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485493e87e3d3b41bc40",
+    "url": "/demo/static/js/main.dbbf1c01.chunk.js"
+  },
+  {
+    "revision": "abc35e6408122337bed0",
+    "url": "/demo/static/js/runtime-main.93868260.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
